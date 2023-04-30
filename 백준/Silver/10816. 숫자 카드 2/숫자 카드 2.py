5cebf0203f4bccf05bea96a2e4e815c7a35308b4
Seong_min <92977540+s-minii@users.noreply.github.com>
2023-05-01 02:32:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 784 ms, Memory: 114132 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,26 +1,20 @@
 # 10816 : 숫자카드 2
 # 풀이
-# 딕셔너리를 통해 키를 카운트하여 총 횟수를 저장하고, 출력한다.
+# 기존 딕셔너리로 찾는 방법 이외에 counter 클래스를 사용한다.
 
-p_cnt = input()
-problem = sorted(map(int, input().split()))
+# counter 클래스를 사용한 방법
+from collections import Counter
 
-u_cnt = input()
+p_cnt = int(input())
+problem = list(map(int, input().split()))
+u_cnt = int(input())
 user = list(map(int, input().split()))
 
-n = {}
+problem = Counter(problem)
 
-# 딕셔너리(n)에 problem 사전의 각 숫자의 등장 횟수를 저장한다.
-# n = {-10: 2, 2: 1, 3: 2, 6: 1, 7: 1, 10: 3}
-
-for i in problem:
-    if i in n:
-        n[i] += 1
-    else :
-        n[i] = 1
 
 for i in user:
-    if i in n:
-        print(n[i], end = ' ')
+    if i in problem:
+        print(problem[i], end = ' ')
     else:
         print('0', end = ' ')
