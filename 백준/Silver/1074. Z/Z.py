958ac22707b1711d9d05f435575ad5b53a1a728a
Seong_min <92977540+s-minii@users.noreply.github.com>
2023-08-21 23:21:31 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,36 @@
+# 1074번 : Z
+# 분류 : 분할정복, 재귀
+# 참고 : https://ggasoon2.tistory.com/11
+
+import sys
+input = sys.stdin.readline
+
+N, r, c = map(int, input().split())
+
+result = 0
+
+while N != 0:
+
+    N -= 1
+
+    # 1사분면
+    if r < 2 ** N and c < 2 ** N:
+        result += (2 ** N) * (2 ** N) * 0
+
+    # 2사분면
+    elif r < 2 ** N and c >= 2 ** N:
+        result += (2 ** N) * (2 ** N) * 1
+        c -= (2 ** N)
+
+    # 3사분면
+    elif r >= 2 ** N and c < 2 ** N:
+        result += (2 ** N) * (2 ** N) * 2
+        r -= (2 ** N)
+
+    # 4사분면
+    else:
+        result += (2 ** N) * (2 ** N) * 3
+        r -= (2 ** N)
+        c -= (2 ** N)
+
+print(result)
