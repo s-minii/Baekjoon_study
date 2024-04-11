1992066d33333f2fb84aff143a75ea33abe5e5e8
Seong_min <yun2044992@naver.com>
2024-04-12 02:36:15 +0900

    5. 정다면체 다른 풀이방법

--- 파이썬 알고리즘 문제풀이 입문/섹션 2. 코드 구현력 기르기/5. 정다면체/5. 정다면체.py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 2. 코드 구현력 기르기/5. 정다면체/5. 정다면체.py
@@ -1,23 +1,44 @@
+# import sys
+# from collections import Counter
+# sys.stdin = open("input.txt", "rt")
+
+# N, M = map(int, input().split())
+# sum_list = []
+# max_sums = []
+#
+# for i in range(1, N+1):
+#     for j in range(1, M+1):
+#         sum_list.append(i+j)
+#
+# result = Counter(sum_list)
+# max_cnt = max(result.values())
+#
+# for sum_val, cnt in result.items():
+#     if cnt == max_cnt:
+#         max_sums.append(sum_val)
+#
+# max_sums.sort()
+#
+# for sum_val in max_sums:
+#     print(sum_val, end=' ')
+
 import sys
-from collections import Counter
-sys.stdin = open("input.txt", "rt")
+#sys.stdin = open("input.txt", "rt")
 
 N, M = map(int, input().split())
-sum_list = []
-max_sums = []
+arr = [0]*(N+M+1)
+max = 0
 
+# 배열에 값 넣는 작업
 for i in range(1, N+1):
     for j in range(1, M+1):
-        sum_list.append(i+j)
+        arr[i+j]+=1
 
-result = Counter(sum_list)
-max_cnt = max(result.values())
+# 가장 큰 값의 인덱스 뽑음
+for i in range(N+M+1):
+    if max < arr[i]:
+        max = arr[i]
 
-for sum_val, cnt in result.items():
-    if cnt == max_cnt:
-        max_sums.append(sum_val)
-
-max_sums.sort()
-
-for sum_val in max_sums:
-    print(sum_val, end=' ')
+for i in range(N+M+1):
+    if max == arr[i]:
+        print(i, end = ' ')
