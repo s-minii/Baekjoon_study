679280062d11535d69452478dfa9dd7a6454a828
Seong_min <yun2044992@naver.com>
2024-04-26 23:30:36 +0900

    3.7 사과나무(다이아몬드) (다른풀이)

--- 파이썬 알고리즘 문제풀이 입문/섹션 3. 탐색 & 시뮬레이션/7. 사과나무(다이아몬드)/7. 사과나무(다이아몬드).py
+++ 파이썬 알고리즘 문제풀이 입문/섹션 3. 탐색 & 시뮬레이션/7. 사과나무(다이아몬드)/7. 사과나무(다이아몬드).py
@@ -3,24 +3,22 @@ sys.stdin = open("input.txt", "rt")
 
 N = int(input())
 arr = []
-center = N // 2
+s=e= N // 2
 result = 0
-sum = 0
 
 for _ in range(N):
     row = list(map(int, input().split()))
     arr.append(row)
 
 for i in range(N):
-    for j in range(N):
+    for j in range(s, e+1):
         result += arr[i][j]
 
-for i in range(center):
-    for j in range(center-i, center+i+1):
-        sum += arr[i][j] #다이아몬드 중앙 + 다이아몬드 위
-        sum += arr[N-i-1][j] #다이아몬드 중앙 + 다이아몬드 아래
-
-sum -= arr[center][center] #다이아몬드 중앙 중복제거
-result -= sum
+    if i<N//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1
 
 print(result)
